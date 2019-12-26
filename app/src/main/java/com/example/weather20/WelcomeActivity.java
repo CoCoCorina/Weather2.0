12d22d421b970ab4f9c7bb1bd01020d0ebb9a692
Corina <1819380652@qq,com>
2019-12-26 15:10:28 +0800

    20191226 15:10

--- app/src/main/java/com/example/weather20/WelcomeActivity.java
+++ app/src/main/java/com/example/weather20/WelcomeActivity.java
@@ -11,9 +11,7 @@ import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 
-/**
- * Created by HUPENG on 2016/9/21.
- */
+
 public class WelcomeActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,7 +22,7 @@ public class WelcomeActivity extends Activity {
         /**标题是属于View的，所以窗口所有的修饰部分被隐藏后标题依然有效,需要去掉标题**/
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_welcome);
-        handler.sendEmptyMessageDelayed(0, 500);
+        handler.sendEmptyMessageDelayed(0, 1800);
     }
 
     @SuppressLint("HandlerLeak")
